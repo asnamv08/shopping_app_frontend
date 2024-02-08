99fced1c341d23656bfdca5fa378ca1e7e6dacf9
asnamv08 <asnamvarghese001@gmail.com>
2024-02-09 02:17:59 +0530

    search created

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -12,6 +12,33 @@ class _itemsearchState extends State<itemsearch> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          appBar: AppBar(title: Text("Item Search"),backgroundColor: Colors.blue.shade300,),
+          body: Container(width: double.infinity,decoration: BoxDecoration(gradient: LinearGradient(colors: [
+            Colors.blue,
+            Colors.green
+          ])),padding: EdgeInsets.all(20),child: Column(children: [
+            TextField(decoration: InputDecoration(labelText: ("Enter Item name"),border: OutlineInputBorder()),),
+            SizedBox(height: 10,),
+            SizedBox(
+              width: 250,
+              height: 60,
+              child: ElevatedButton(style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.blue
+              ),onPressed: (){}, child: Text("Search")),
+            ),
+            SizedBox(height: 10,),
+            SizedBox(
+              width: 250,
+              height: 60,
+              child: ElevatedButton(style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.blue
+              ),onPressed: (){
+                Navigator.pop(context);
+              }, child: Text("Back")),
+            )
+          ]),)
 
       ),
     );
